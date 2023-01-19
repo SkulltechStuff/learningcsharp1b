a0002997b628fc0c61bb5c5c166c1989d37aa819
SkulltechStuff <rt652@student.aru.ac.uk>
2023-01-19 14:25:35 +0000

    firstcommit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace learningcsharp1;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string username = "Kat";
+
+        Console.WriteLine("Hello, User!");
+        Console.WriteLine("Please enter your name");
+        username = Console.ReadLine();
+        Console.WriteLine($"Goodbye, {username}!");
+        Console.ReadKey(true);
+    }
+}
